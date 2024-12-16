fb4c293b0de1488bb98bdcf40d471784bfb74ddf
ytreeshan <tjainarine@gmail.com>
2024-12-16 07:21:15 -0500

    Added Number and Login page to NavBar added homepage links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const NavBar = () => {
+
+    const user = false;
     return (
     <div className="h-12 text-red-500 px-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase text-sm md:text-base cursor-pointer">
+        {
+            /*Left Links*/ 
+        }
+        <div className= "hidden md:flex gap-4"> 
+            <Link href="/">HomePage</Link>
+            <Link href="/">Menu</Link>
+            <Link href="/">Contact</Link>
+
+        </div>
+        
+        
         {
             /*Logo*/ 
         }
@@ -16,9 +31,28 @@ const NavBar = () => {
         {
             /*logo*/
         }
-        <div>
+        <div className="hidden">
         <Menu/>
         </div>
+        {
+            /*Right Links*/
+        }
+        <div className="hidden md:flex gap-4">
+                <Image src="/phone.png" alt=" " width={20} height={20} />
+    
+                <span>123-456-7890</span>
+
+            </div>
+
+            {!user ? (
+            
+            <Link href="/login">Login</Link>
+            ) : (
+            <Link href="/orders">Orders</Link>
+            )}
+            <CartIcon/>
+            
+            
     </div>
     );
 }
