11a2ca3a5515d13bec39e4a16a2ac6e43ff3ecb3
ytreeshan <tjainarine@gmail.com>
2024-12-17 10:20:43 -0500

    added bav item  and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
     return (
 
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+    <div className="h-12 md:h-24 px-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
         
         <Link href="/" className="font-bold text-xl:">MASSIMO</Link>
         <p>© ALL RIGHTS RESERVED</p>
